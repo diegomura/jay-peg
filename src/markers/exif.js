472cc73d9c38cd04f78d6715a65abb82af88d4b4
Jerry Tan <66892505+Friendseeker@users.noreply.github.com>
2024-09-20 01:53:49 -0700

    fix: buffer start offset (#11)
    * Fix start offset
    
    * Fix exifIDF offset

--- src/markers/exif.js
+++ src/markers/exif.js
@@ -251,11 +251,12 @@ class IDFEntries {
       return {};
     }
 
-    const entries = this._decodeIDFEntries(buffer, tags.ifd, offsetToFirstIFD);
+    const firstIFDBuffer = buffer.slice(offsetToFirstIFD - 8);
+    const entries = this._decodeIDFEntries(firstIFDBuffer, tags.ifd, offsetToFirstIFD);
     const { exifIFDPointer, gpsInfoIFDPointer } = entries;
 
     if (exifIFDPointer) {
-      buffer = buffer.slice(exifIFDPointer - offsetToFirstIFD);
+      buffer = buffer.slice(exifIFDPointer - 8);
       entries.subExif = this._decodeIDFEntries(
         buffer,
         tags.ifd,
